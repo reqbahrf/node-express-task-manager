b8a348a7b0c8d312d388471eac237aa7095861cf
Reanz_Arthur <reqbahrf@gmail.com>
2025-08-14 23:46:53 +0800

    Initialize Node.js application

---
+++ app.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const connectDB = require('./db/connect');
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+
+app.use(express.json());
+const port = 3000;
+
+app.use('/api/v1/tasks', tasks);
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on port: ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
